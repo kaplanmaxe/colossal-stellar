61a9b9bf4014454132bcad466e4c00c62f84b7a2
Max Kaplan <kaplanmaxe3@gmail.com>
2018-01-16 23:08:34 -0500

    inital functionality done

---
+++ __tests__/stellar-test.js
@@ -0,0 +1,34 @@
+import {
+  genKeypair,
+  getKeypairFromPrivateKey,
+  getBalance,
+  sendTransaction,
+} from '../src/';
+
+test('Keypair should generate successfully', () => {
+  const keypair = genKeypair();
+  expect(keypair.publicKey().length).toBe(56);
+  expect(keypair.secret().length).toBe(56);
+});
+
+test('Get public key from secret', () => {
+  const keypair = getKeypairFromPrivateKey('SABRMBLU222ITHT22PEXDC434RYZWLD6TVXJ4ZPO4HNPJ3UBBO3BYE5U');
+  expect(keypair.publicKey()).toBe('GBOUQQ3PZULFVMY7GQRXEGNABSWVWQ6PRM3TGVDEALSSFUH75NAJW4QU');
+});
+
+test('Check balance of wallet', () => {
+  expect.assertions(1);
+  return getBalance('GCNGOYIEGGF24TSHNMP5W62T3IURL4B7DLOMQYMPDEN5AGN6BLUQWY4I', true)
+    .then((res) => expect(Number(res[0].balance)).toBeGreaterThanOrEqual(10000));
+});
+
+test('Send transaction', () => {
+  jest.setTimeout(10000);
+  expect.assertions(1);
+  return sendTransaction(
+    'SCC2L2KAEP3SVLX2KZL6SXCYPDPTEZAFGRW25ASX6UC6AUUKJ3GL3ZPK',
+    'GCNGOYIEGGF24TSHNMP5W62T3IURL4B7DLOMQYMPDEN5AGN6BLUQWY4I',
+    '0.0001',
+    true,
+  ).then((res) => expect(res.hash).not.toBe(undefined));
+});
---
+++ src/index.js
@@ -0,0 +1,147 @@
+import StellarSdk from 'stellar-sdk';
+import request from 'request';
+
+/**
+ * Generate new Stellar keypair
+ */
+export function genKeypair() {
+  return StellarSdk.Keypair.random();
+}
+
+/**
+ * Return keypair from privateKey
+ * @param {string} privateKey
+ */
+export function getKeypairFromPrivateKey(privateKey) {
+  return StellarSdk.Keypair.fromSecret(privateKey);
+}
+
+/**
+ * Instantiate server object for either testnet or mainnet
+ * @param {boolean} testnet Connect to testnet
+ * @return {StellarSdk.Server}
+ */
+export function connect(testnet = false) {
+  return new StellarSdk.Server(testnet === true ? 'https://horizon-testnet.stellar.org' : 'https://horizon.stellar.org');
+}
+
+/**
+ * Select network to use
+ * @param {boolean} testnet Use testnet
+ */
+export function selectNetwork(testnet = false) {
+  if (testnet === true) {
+    StellarSdk.Network.useTestNetwork();
+  } else {
+    StellarSdk.Network.usePublicNetwork();
+  }
+}
+
+/**
+ * Ask friendbot for Lumens
+ * @param address Address to send lumens to
+ */
+export function callFaucet(address) {
+  return new Promise((resolve, reject) => {
+    request.get({
+      url: 'https://horizon-testnet.stellar.org/friendbot',
+      qs: { addr: address },
+      json: true,
+    }, (err, response, data) => {
+      if (err || response.statusCode !== 200) { return reject({ err: data.detail }); }
+      resolve({ hash: data.hash, ledger: data.ledger });
+    });
+  });
+}
+
+/**
+ * Check balance of address
+ * @param {string} address address to check
+ * @param {boolean} testnet
+ */
+export async function getBalance(address, testnet = false) {
+  try {
+    const server = connect(testnet);
+    const { balances } = await server.loadAccount(address);
+    return balances;
+  } catch (e) {
+    console.error(e.message.detail);
+  }
+}
+
+/**
+ * Send a transaction to an address
+ * @param {string} privateKey Private key of wallet to send from
+ * @param {string} destination Receiver of funds
+ * @param {string} amount Amount to send
+ * @param {boolean} testnet
+ */
+export async function sendTransaction(privateKey, destination, amount, testnet = false) {
+  if (!privateKey || !destination || !amount) {
+    throw new Error('An address, amount, and private key must be specified');
+  }
+
+  try {
+    selectNetwork(testnet);
+    const server = connect(testnet);
+    const keypair = getKeypairFromPrivateKey(privateKey);
+    const account = await server.loadAccount(keypair.publicKey());
+    const transaction = createTransaction(account, destination, amount, keypair);
+    const { hash, ledger } = await server.submitTransaction(transaction);
+    return { hash, ledger };
+  } catch (e) {
+    console.error(e.data.data.detail);
+  }
+}
+
+/**
+ * Construct Transaction
+ * @param {object} account Account to send from
+ * @param {object} destination Destination to send funds to
+ * @param {object} amount Amount of funds to send
+ * @param {object} keypair Keypair object of sender
+ */
+export function createTransaction(account, destination, amount, keypair) {
+  const transaction = buildTransaction(account, destination, amount);
+  // Sign transaction
+  transaction.sign(keypair);
+  return transaction;
+}
+
+/**
+ * Build transaction object
+ * @param {string} account Account to send from
+ * @param {string} destination Destination to send funds to
+ * @param {string} amount Amount of funds to send
+ */
+export function buildTransaction(account, destination, amount) {
+  return new StellarSdk.TransactionBuilder(account)
+    .addOperation(StellarSdk.Operation.payment({
+      asset: StellarSdk.Asset.native(),
+      destination,
+      amount,
+    }))
+    .build();
+}
+
+/**
+ * Vote for inflation lumens
+ */
+export async function voteForInflationLumens(secret, destination) {
+  selectNetwork(false);
+  const server = connect(false);
+  const keypair = getKeypairFromPrivateKey(secret);
+  try {
+    const account = await server.loadAccount(keypair.publicKey());
+    const transaction = new StellarSdk.TransactionBuilder(account)
+      .addOperation(StellarSdk.Operation.setOptions({
+        inflationDest: destination,
+      }))
+      .build();
+    transaction.sign(keypair);
+    const { hash, ledger } = await server.submitTransaction(transaction);
+    return { hash, ledger };
+  } catch (e) {
+    console.error(e.data.extras.result_codes);
+  }
+}
